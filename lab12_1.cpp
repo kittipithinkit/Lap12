ddb0027afe61169fe8010e43b3ac582c6260233a
kittipithinkit <kittipithm@gmail.com>
2026-01-06 16:07:25 +0700

    fibo50

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+long long fibonacci(int x) {
+    if (x == 0) return 0;
+    if (x == 1) return 1;
+    long long a = 0, b = 1, c;
+    for (int i = 2; i <= x; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main() {
+    int x = 50;
+    cout << "Fibonacci(50) = " << fibonacci(x) << endl;
+    return 0;
 }
